ba4f69117d21681e1d392673f4c23078e797a9bd
gabrielrodrigues25 <goliveira0321@gmail.com>
2026-05-02 16:45:42 -0300

    Att disp login 2

--- public/pages/1.home/js/home.js
+++ public/pages/1.home/js/home.js
@@ -2,9 +2,9 @@ fetch(`/usuario`)
   .then(res => res.json())
   .then(user => {
     document.getElementById("welcome").innerText =
-      `Seja bem-vindo, ${user.nome}!`;
+      `Seja Bem-Vindo, ${user.nome}!`;
   })
   .catch(() => {
     document.getElementById("welcome").innerText =
-      "Seja bem-vindo!";
+      "Seja Bem-Vindo!";
   });
